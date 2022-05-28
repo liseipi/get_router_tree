3f172c2cc491cda7b1dfc9f8547b6291099c0326
sakura <sakura@qq.com>
2022-05-28 21:36:03 +0800

    fix router

--- lib/app/screen/pages/index/index_page.dart
+++ lib/app/screen/pages/index/index_page.dart
@@ -15,6 +15,7 @@ class IndexPage extends GetView<IndexController> {
         // print(delegate.currentConfiguration);
         // print(delegate.navigatorObservers);
         // print(delegate.build(context));
+        print(GetDelegate());
 
         // final delegate = context.navigation;
         //This router outlet handles the appbar and the bottom navigation bar
@@ -30,122 +31,46 @@ class IndexPage extends GetView<IndexController> {
           body: GetRouterOutlet(
             anchorRoute: '/index',
             initialRoute: '/index/home',
-
-            //delegate: Get.nestedKey(Routes.HOME),
-            // key: Get.nestedKey(Routes.HOME),
           ),
-          // body: Column(
-          //   children: [
-          //     const Text("Index page"),
-          //     ElevatedButton(
-          //       onPressed: () {
-          //         Get.toNamed('/setting');
-          //       },
-          //       child: const Text("GO Setting Page"),
-          //     ),
-          //   ],
+          // floatingActionButton: FloatingActionButton(
+          //   onPressed: () {},
+          //   child: const Text("+"),
           // ),
-          // body: GetRouterOutlet(
-          //   anchorRoute: '/index',
-          //   initialRoute: '/home',
-          //   delegate: delegate,
-          //   // key: Get.nestedKey(2),
-          //   // filterPages: (afterAnchor) {
-          //   //   print(afterAnchor);
-          //   //   print('dddddddddddddddddd');
-          //   //   print(afterAnchor.take(1));
-          //   //   return afterAnchor.take(1);
-          //   // },
-          // ),
-          floatingActionButton: FloatingActionButton(
-            onPressed: () {},
-            child: const Text("===="),
-          ),
           bottomNavigationBar: BottomNavigationBar(
             currentIndex: currentIndex,
             onTap: (value) {
-              print(value);
               switch (value) {
                 case 0:
                   delegate.toNamed('/index/home');
                   break;
                 case 1:
-                  delegate.toNamed('/account');
-                  // delegate.toNamed('/index/account');
+                  delegate.toNamed('/index/account');
+                  // delegate.toNamed('/account');
                   break;
                 default:
               }
             },
             items: const [
-              // _Paths.HOME + [Empty]
               BottomNavigationBarItem(
-                // icon: Icon(Icons.home),
                 icon: Padding(
                   padding: EdgeInsets.only(top: 28, bottom: 5),
                   child: Icon(Icons.home),
                 ),
                 label: 'Home',
               ),
-              // _Paths.HOME + Routes.PROFILE
               BottomNavigationBarItem(
-                // icon: Icon(Icons.account_box_rounded),
                 icon: Padding(
                   padding: EdgeInsets.only(top: 28, bottom: 5),
                   child: Icon(Icons.account_box_rounded),
                 ),
-                label: 'Profile',
+                label: 'Account',
               ),
             ],
           ),
 
-          // bottomNavigationBar: BottomNavigationBar(
-          //   currentIndex: 0,
-          //   items: const [
-          //     BottomNavigationBarItem(
-          //       backgroundColor: Colors.blue,
-          //       icon: Icon(Icons.home),
-          //     ),
-          //     BottomNavigationBarItem(
-          //       backgroundColor: Colors.red,
-          //       icon: Icon(Icons.person),
-          //     ),
-          //   ],
-          // ),
         );
       },
     );
-    // return GetRouterOutlet(
-    //   anchorRoute: '/',
-    //   initialRoute: '/index',
-    //   // delegate: GetDelegate(),
-    //   key: Get.nestedKey(null),
-    // );
-    // return RouterOutlet.builder(
-    //   delegate: Get.nestedKey(null),
-    //   builder: (context) {
-    //     return Scaffold();
-    //     // final title = context.location;
-    //     // return Scaffold(
-    //     //   drawer: DrawerWidget(),
-    //     //   appBar: AppBar(
-    //     //     title: Text(title),
-    //     //     centerTitle: true,
-    //     //   ),
-    //     //   //body: HomeView(),
-    //     //
-    //     //   body: GetRouterOutlet(
-    //     //     initialRoute: Routes.HOME,
-    //     //     delegate: Get.nestedKey(null),
-    //     //     anchorRoute: '/',
-    //     //     filterPages: (afterAnchor) {
-    //     //       // print(afterAnchor);
-    //     //       // print('dddddddddddddddddd');
-    //     //       // print(afterAnchor.take(1));
-    //     //       return afterAnchor.take(1);
-    //     //     },
-    //     //   ),
-    //     // );
-    //   },
-    // );
+
   }
 }
