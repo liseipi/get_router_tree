803c5f9cac4ba1fc54d404641082bb2443b31ab2
sakura <sakura@qq.com>
2022-05-29 01:30:39 +0800

    fix router

--- lib/app/screen/root/root_page.dart
+++ lib/app/screen/root/root_page.dart
@@ -15,68 +15,15 @@ class RootPage extends GetView<RootController> {
           body: GetRouterOutlet(
             anchorRoute: '/',
             initialRoute: '/index',
-            delegate: delegate,
+            // delegate: delegate,
             // key: Get.nestedKey(1),
-            // filterPages: (afterAnchor) {
-            //   print(afterAnchor);
-            //   print('dddddddddddddddddd');
-            //   print(afterAnchor.take(1));
-            //   return afterAnchor.take(1);
-            // },
           ),
-          floatingActionButton:  FloatingActionButton(
-              // FloatingActionButtonLocation:
-              onPressed: () {  },
-            child: const Text("++"),
-          ),
-          // bottomNavigationBar: BottomNavigationBar(
-          //   currentIndex: 0,
-          //   items: const [
-          //     BottomNavigationBarItem(
-          //       backgroundColor: Colors.blue,
-          //       icon: Icon(Icons.home),
-          //     ),
-          //     BottomNavigationBarItem(
-          //       backgroundColor: Colors.red,
-          //       icon: Icon(Icons.person),
-          //     ),
-          //   ],
+          // floatingActionButton: FloatingActionButton(
+          //   onPressed: () {},
+          //   child: const Text("++"),
           // ),
         );
       },
     );
-    // return GetRouterOutlet(
-    //   anchorRoute: '/',
-    //   initialRoute: '/index',
-    //   // delegate: GetDelegate(),
-    //   key: Get.nestedKey(null),
-    // );
-    // return RouterOutlet.builder(
-    //   delegate: Get.nestedKey(null),
-    //   builder: (context) {
-    //     return Scaffold();
-    //     // final title = context.location;
-    //     // return Scaffold(
-    //     //   drawer: DrawerWidget(),
-    //     //   appBar: AppBar(
-    //     //     title: Text(title),
-    //     //     centerTitle: true,
-    //     //   ),
-    //     //   //body: HomeView(),
-    //     //
-    //     //   body: GetRouterOutlet(
-    //     //     initialRoute: Routes.HOME,
-    //     //     delegate: Get.nestedKey(null),
-    //     //     anchorRoute: '/',
-    //     //     filterPages: (afterAnchor) {
-    //     //       // print(afterAnchor);
-    //     //       // print('dddddddddddddddddd');
-    //     //       // print(afterAnchor.take(1));
-    //     //       return afterAnchor.take(1);
-    //     //     },
-    //     //   ),
-    //     // );
-    //   },
-    // );
   }
 }
